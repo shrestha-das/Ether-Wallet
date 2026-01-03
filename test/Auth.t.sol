40c86dc39b8efd6a55df3f78c130a26751cf9717
email <shresthadas.0.infinity@gmail.com>
2026-01-03 13:34:23 +0000

    used Foundry, modified code, wrote test

---
+++ test/Auth.t.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import {Test} from "forge-std/Test.sol";
+import {EtherWallet} from "src/EtherWallet.sol";
+
+contract AuthTest is Test {
+    EtherWallet public etherWallet;
+
+    function setUp() external {
+        etherWallet = new EtherWallet();
+    }
+
+    function testSetOwner() external {
+        etherWallet.setOwner(address(1));
+        assertEq(etherWallet.owner(),address(1));
+    }
+
+    function test_ReverIf_NotOwner() external {
+        vm.prank(address(2));
+        vm.expectRevert();
+        etherWallet.setOwner(address(1));
+    }
+
+    function test_RevertIf_SetOwnerAgain() external {
+        // msg.sender = address(this).balance;
+        etherWallet.setOwner(address(1));
+
+        vm.startPrank(address(1));
+
+        etherWallet.setOwner(address(1));
+        etherWallet.setOwner(address(1));
+        etherWallet.setOwner(address(1));
+
+        vm.stopPrank();
+
+        // msg.sender = address(this).balance;
+    }
+}
